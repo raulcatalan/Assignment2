6014203cc838cf243a2c14efc86875e2396ac57c
Raul Catalan <rcatala1@asu.edu>
2019-10-09 16:42:03 -0700

    Updated the AddingMachine and create a main for testing purposes

--- Project2/src/cse360assign2/AddingMachine.java
+++ Project2/src/cse360assign2/AddingMachine.java
@@ -4,28 +4,37 @@ package cse360assign2;
 public class AddingMachine {
 
 	private int total;
+	private String history;
 	
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
+		history = "0";
 	}
 	
+	//returns int total
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	public void add (int value) {
-		
+		//total is added with the value, string history is append what was added to total.
+		total += value;
+		history += " + " + value;
 	}
 	
 	public void subtract (int value) {
-		
+		//total is subtracted with the value, string history is append what was subtracted to total.
+		total -= value;
+		history += " - " + value;
 	}
 		
 	public String toString () {
-		return "";
+		return history;
 	}
-
-	public void clear() {
 	
+	//clears the value in total and sets history = 0 for new log of functions
+	public void clear() {
+		total = 0;
+		history = "0";
 	}
 }
